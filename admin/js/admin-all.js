6922913014cb4ac66aa56a6fcf0f872b2310ccb4
ykioapr <ykioapr@gmail.com>
2025-07-05 00:00:54 +0800

    Upload by PicGo

---
+++ admin/js/admin-all.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+    $("#theme_act_btn").click(function(t){var a=$(this),e=a.data("text");a.text("Loading..."),a.attr("disabled",!0),$.post(zb.ajax_url,{action:"theme_ajax_activ",id:$("#theme_lic_id").val(),key:$("#theme_lic_key").val()},function(t){a.text(t.msg),setTimeout(function(){a.text(e),a.attr("disabled",!1),1==t.status&&confirm("激活成功，是否刷新当前页面？")&&location.reload()},2e3)})});
+});
